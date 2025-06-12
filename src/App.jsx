14dfb3c706c41724430bffc3b9af0ecdbf10d930
lucas <lucassouzafranco1@gmail.com>
2025-06-12 12:57:52 -0300

    feat: Footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Menu from './components/Menu/Menu'
 import Content from './components/Content/Content'
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
       <div className='appContainer'>
         <Menu />
         <Content />
+        <Footer />
       </div>
       
     </>
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import './Footer.css';
+
+export default function Footer() {
+    return(
+        <>
+            <div className="footerContainer">
+                <p>6315</p>
+                <p>7001</p>
+                <p>7016</p>
+                <p>7024</p>
+                <p>8118</p>
+            </div>
+        </>
+    )
+}
