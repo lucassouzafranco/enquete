e651a785bbb2730ff442cf48170af43c38b734a1
rodrigojeremiasufv <rodrigo.jeremias@ufv.br>
2025-06-23 12:37:11 -0300

    update the build script

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
 STACK_NAME="votacao_stack"
+
 echo "Buildando as imagens"
-docker-compose build
+docker build -t urna_service:latest ./votacao_sistema/urna
+docker build -t agregador_service:latest ./votacao_sistema/agregador
+
 echo "Fazendo o deploy"
 docker stack deploy -c docker-compose.yml $STACK_NAME
+
 echo "Status:"
 docker stack services $STACK_NAME
